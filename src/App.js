f31a35ffedc424889dd60085a1ed61731279bf80
Siva Neredupalli <neredupallisiva3@gmail.com>
2025-10-01 12:35:46 +0530

    Wildcard error handling & Middlewares

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 const express = require("express")
 const app = express();
-const {adminAuth,userAuth}= require("./middlewares/auth.js")
-app.use('/admin',adminAuth)
-app.use('/user',userAuth)
-app.get('/user/getAllData',(req,res,)=>{
-    res.send("User all data got successfully")
+
+
+app.get("/getUser",(req,res)=>{
+  try{
+    throw new Error("some error occured")
+    res.send("UserData getting successfully")
+  }
+  catch(err){
+    res.status(500).send("Something went wrong")
+  }
 })
-  app.get('/user/deleteUser',(req,res,)=>{
-    res.send("User data deleted successfully")
-})
-app.get('/admin/getAllData',(req,res)=>{
-  res.send("adminuser data got successfully");
-})
-app.get('/admin/deleteUser',(req,res)=>{
-  res.send("AdminUser details deleted successfully")
+app.use("/",(err,req,res,next)=>{
+  if(err){
+    res.status(500).send("Getting Unexpected Error")
+  }
+  next()
 })
 app.listen(3333,()=>{
     console.log("Server successfully listening the port 3333 requests")
